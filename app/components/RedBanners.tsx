06271e625a67445cb70b133a01584d2499cbe81a
Blissito <fixtergeek@gmail.com>
2025-02-17 10:35:09 -0600

    read banners

--- app/components/RedBanners.tsx
+++ app/components/RedBanners.tsx
@@ -1,9 +1,4 @@
-import {
-  motion,
-  useAnimate,
-  useMotionTemplate,
-  useSpring,
-} from "framer-motion";
+import { motion, useAnimate, useMotionTemplate, useSpring } from "motion/react";
 import {
   Children,
   useEffect,
@@ -17,32 +12,36 @@ import { useInfinityMovement } from "~/hooks/useInfinityMovement";
 export const RedBanners = ({ children }: { children?: ReactNode }) => {
   const firstChild = Children.toArray(children)[0];
   const secondChild = Children.toArray(children)[1];
+  const [currentHover, setCurrentHover] = useState(1);
+
   const x = useSpring(500);
   const y = useSpring(500);
+
   const background = useMotionTemplate`radial-gradient(at ${x}px ${y}px, #5158f6 5%, black 80%)`;
-  const [currentHover, setCurrentHover] = useState(1);
+
   const handleMouseMove = (event: MouseEvent<HTMLElement>) => {
-    x.set(event.pageX);
-    y.set(event.pageY);
+    const { pageY, pageX } = event;
+    x.set(pageX);
+    y.set(pageY);
   };
 
   return (
     <motion.article
-      onMouseMove={handleMouseMove}
       style={{ background }}
+      onMouseMove={handleMouseMove}
       className={cn("min-h-screen text-white relative overflow-hidden")}
     >
       <BigRectangle
         onMouseEnter={() => setCurrentHover(0)}
-        isHovered={currentHover === 0}
+        isHover={currentHover === 0}
         className="rotate-12 bg-black"
       >
         {secondChild}
       </BigRectangle>
       <BigRectangle
         direction={-1}
-        isHovered={currentHover === 1}
         onMouseEnter={() => setCurrentHover(1)}
+        isHover={currentHover === 1}
       >
         {firstChild}
       </BigRectangle>
@@ -54,39 +53,37 @@ const BigRectangle = ({
   children,
   className,
   onMouseEnter,
-  direction,
-  isHovered,
+  isHover,
+  direction = 1,
 }: {
   direction?: number;
-  onMouseEnter: () => void;
-  isHovered: boolean;
+  isHover?: boolean;
+  onMouseEnter?: () => void;
   className?: string;
   children?: ReactNode;
 }) => {
+  const [scope, animate] = useAnimate();
   const [infinityScope, x] = useInfinityMovement(direction);
-  const [animateScope, animate] = useAnimate();
-
   useEffect(() => {
-    if (isHovered) {
-      animate(animateScope.current, { filter: "blur(0px)" }, { duration: 0.5 });
+    if (isHover) {
+      animate(scope.current, { filter: "blur(0px)" }, { duration: 0.5 });
     } else {
-      animate(animateScope.current, { filter: "blur(9px)" }, { duration: 2 });
+      animate(scope.current, { filter: "blur(9px)" }, { duration: 2 });
     }
-  }, [isHovered]);
+  }, [isHover]);
   return (
     <div
-      ref={animateScope}
-      onMouseEnter={onMouseEnter}
       style={{ filter: "blur(9px)" }}
+      ref={scope}
+      onMouseEnter={onMouseEnter}
       className={cn(
         "h-28 bg-red-500 absolute -rotate-12 top-[40vh] w-[120%] translate-x-[-10vw] gird place-content-center text-3xl text-nowrap",
         "uppercase font-bold",
         className
       )}
     >
-      <motion.div ref={infinityScope} style={{ x }} className="w-max mx-4">
-        {children}
-        {children}
+      <motion.div style={{ x }} ref={infinityScope} className="w-max flex">
+        {children} {children}
       </motion.div>
     </div>
   );
